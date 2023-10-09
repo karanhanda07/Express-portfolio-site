66df8d0ba355598cc2c29cfe2658e73b390809ac
karanhanda07 <karanhanda40@yahoo.com>
2023-10-08 21:16:48 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
